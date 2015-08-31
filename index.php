c4a76d4f00fb8ec5efd4375fac642a4e687220c6
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2015-08-31 11:07:53 +0200

    index.php
    Homepage

---
+++ index.php
@@ -0,0 +1,286 @@
+<?php 
+include('functions.php');
+include('header.php'); 
+?>
+
+    <main role="main" class="index row">
+
+    <h1 class="col-10 col-offset-1">Relache 2015 : Musique itinérante !</h1>
+        
+        <article class="article-slider col-10 col-offset-1">
+
+        <a href="programmation.php#septembre">
+
+            <div class="your-class">
+
+
+
+                <!--<div class="row">
+
+                    <div class="slider-img col-m-8">
+                        <img src="img/slider/slidemain.png" alt="Relache 2015 Bordeaux">
+                    </div><!-- hack pour safari
+                 <div class="slider-right col-12 col-center col-m-4 basic">
+
+                        <label class="date">26 JUIN<br>5 SEPT.<br></label>
+                        <h2>Bordeaux</h2>
+
+                        <h1>&mdash;<br>Relache 2015<br>&mdash;</h1>
+                        
+                        <h3>Concerts</h3>
+                        <h3>Siestes Soul</h3>
+                        <h3>Dancing in the Street</h3>
+
+                    </div>
+
+                </div>-->
+
+
+                <div class="row">
+
+                    <div class="slider-img col-12">
+                        <img src="img/slider/28-08.png" title="Accès à la programmation de Relache 2015 Bordeaux" alt="28 Août Relache 2015 Bordeaux" class="right">
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="slider-img col-12">
+                        <img src="img/slider/04-09-dancing.png" title="Accès à la programmation de Relache 2015 Bordeaux" alt="04 Septembre Dancing in the Street Relache 2015 Bordeaux" class="right">
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="slider-img col-12">
+                        <img src="img/slider/05-09-dancing.png" title="Accès à la programmation de Relache 2015 Bordeaux" alt="05 Septembre Dancing in the Street Relache 2015 Bordeaux" class="right">
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="slider-img col-12">
+                        <img src="img/slider/12-09.png" title="Accès à la programmation de Relache 2015 Bordeaux" alt="12 Septembre cloture Relache 2015 Bordeaux" class="right">
+                    </div>
+
+                </div>
+
+                <div class="row">
+                
+                    <div class="slider-img col-12">
+                        <img src="img/slider/soul.jpg" title="Accès à la programmation de Relache 2015 Bordeaux" alt="Siestes Soul Relache 2015 Bordeaux" class="right">
+                    </div>
+
+                </div>
+              
+            </div>
+
+
+        </a>         
+            
+        </article>
+
+        <article class="article-newsletter col-10 col-offset-1">
+
+            <form action="newsletter.php" method="post">
+
+                    <label class="col-12 col-m-6">>> Abonnez-vous à la Newsletter</label>
+
+                    <div class="email col-12 col-m-6 row">
+                   
+                        <input class="col-6 col-offset-3" type="email" name="email" id="email" placeholder="&nbsp;&nbsp;Adresse e-mail">
+
+                        <input class="col-2 col-offset-1" type="submit" value="GO">
+
+                    </div>
+
+            </form>
+
+        </article>
+
+        <article class="programmation col-10 col-offset-1 col-m-4">
+            
+            <a href="programmation.php#juillet">
+                <h2>Prochainement à Relache 2015</h2>
+
+                <div class="row">
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/28-08.png" alt="Relache 28 Août 2015 Concert" width="100%">
+                    </div>
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/02-09-sieste.png" alt="Relache 02 Septembre 2015 Sieste Soul" width="100%">
+                    </div>
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/03-09-sieste.png" alt="Relache 03 Septembre 2015 Sieste Soul" width="100%">
+                    </div>
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/04-09-dancing.png" alt="Relache 04 Septembre 2015 Dancing in the Street" width="100%">
+                    </div>
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/05-09-dancing.png" alt="Relache 05 Septembre Dancing in the street" width="100%">
+                    </div>
+
+                    <div class="zone-img col-6 col-m-4">
+                        <img src="img/programmation/12-09.png" alt="Relache 12 Septembre 2015 Concert" width="100%">
+                    </div>
+
+                </div>
+            </a>
+
+        </article>
+
+        <article class="article-relache col-10 col-offset-1 col-m-5">
+
+            <h2><a href="contact.php"> Qu'est-ce que Relache&nbsp;?</a></h2>
+
+                <p>
+                    À Paris, il y a Paris-Plage. À Bordeaux, nous c’est RELACHE ! Un vrai
+festival urbain et en kit, à assembler soi-même de Juin à Septembre. Pas de problème de douche ou bien même de t-shirt de 3 jours : l’ambiance festival est calée en plein centre-ville le temps d’un soir et vous êtes chez vous dans l’heure. 15 dates de
+concerts, près de 10 Dancing in the street et de douces après midi avec les Siestes Soul sur les Quais et au Jardin Public.
+   <br>La programmation, historiquement dédiée aux musiques rock, avec des groupes tels que Jim Jones Revue, Ty Segall, Thee Oh Sees et Lisa &amp; The Lips
+                </p>
+                  <br>
+                  <a class="btn" href="contact.php">>> Lire plus +</a>
+
+        </article>
+
+        <aside class="col-10 col-offset-1 col-m-4">
+
+            <h2>Suivez-nous !</h2>
+
+            <section class="playlist">
+
+                <h3>
+                    <a href="https://soundcloud.com/relache-1" target="_blank">
+                        <img src="img/icons/vinyl.svg" alt="" width="30px" style="vertical-align: middle">&nbsp;&nbsp;La playlist relache
+                    </a>
+                </h3>
+
+                <iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/129199530&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+            </section>
+
+            <section class="instagramlive">
+
+                <h3>
+                    <a href="https://instagram.com/relachefestival/" target="_blank">
+                        <img src="img/icons/yeu.svg" alt="" width="30px" style="vertical-align: middle">&nbsp;&nbsp;Instagram #relache2015
+                    </a>
+                </h3>
+
+                <div id="instafeed"></div>
+
+            </section>
+
+        </aside>
+
+        <article class="article-actualite col-10 col-m-5 col-offset-1">  
+
+            <h2 class="titre-blog">Carnet de voyage</h2>              
+
+            <?php 
+
+            $articles = get_articles();
+
+            foreach ($articles as $article): 
+
+                $thumbnail = get_thumbnail($article['ID']);
+                $author = get_author($article['post_author']);
+            ?>            
+
+            <section class="article-blog col-m-12">
+
+                <div class="row">
+
+                        <div class="thumbnail col-m-4">
+
+                            <?php if ($thumbnail['guid']) : ?>
+                    
+                            <img src="<?= $thumbnail['guid'] ?>" title ="image à la une du Blog de Relache 2015" alt="image à la une d'article du blog de Relache 2015">
+                            
+                            <?php else : ?>
+
+                            <img src="img/icons/icontest.jpg" title ="image à la une du Blog de Relache 2015"alt="image à la une d'article du blog de Relache 2015">
+
+                            <?php endif ?>
+
+                        </div><!--Obligé sinon ça pète !
+
+                        --><div class="article col-12 col-m-8">
+
+                            <h3><?= $article['post_title']; ?></h3>
+
+                            <em><?= $article['post_date']; ?> - <?= $author['display_name']; ?></em>
+
+                            <p><?= $article['post_excerpt']; ?></p>
+                            <a class="btn btn-small" href="<?= $article['guid']; ?>"> >> Lire plus +</a>
+                            
+                        </div>
+
+                </div>
+
+            </section>
+
+            <?php endforeach ?>
+
+        </article>
+
+        <article class="article-galerie col-10 col-offset-1">
+
+            <h2>Ambiance Relache</h2>
+
+            <section class="galerie row">
+                
+                <div class="zone-img col-m-6 col-l-4">
+
+                    <a href="galerie.php">
+                        <img src="img/galerie/concert/concert.jpg" alt="galerie relache 2015 bordeaux">
+
+                        <p class="concert">Concerts <br>
+                        Relache 2015</p>
+                        
+                        <div class="hover-img concert"></div>
+                    </a>
+                </div><!--
+
+             --><div class="zone-img col-m-6 col-l-4">
+
+                    <a href="galerie.php">
+                        <img src="img/galerie/dancing/dancing.jpg" alt="galerie relache 2015 bordeaux">
+                        
+                        <p class="dancing">Dancing in the street <br>
+                        Relache 2015</p>
+                        
+                        <div class="hover-img dancing"></div>
+                    </a>
+
+                </div><!--
+
+             --><div class="zone-img col-l-4">
+
+                    <a href="galerie.php">
+                        <img src="img/galerie/soul/soul.jpg" alt="galerie relache 2015 bordeaux">
+
+                        <p class="soul">Siestes Soul <br>
+                        Relache 2015</p>
+                        
+                        <div class="hover-img soul"></div>
+                    </a>
+
+                </div>
+
+            </section>
+
+        </article>
+        
+    </main> <!-- .l-constrained -->
+    
+
+<?php include('footer.php'); ?>
